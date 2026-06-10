369b90307c8a9c41850bd365adcf3ca924591d95
Daniel Njeru <danofficial014@gmail.com>
2026-06-10 20:25:50 +0300

    Create premium portfolio

---
+++ script.js
@@ -0,0 +1,14 @@
+window.addEventListener("scroll", () => {
+
+```
+const navbar = document.querySelector(".navbar");
+
+if(window.scrollY > 50){
+    navbar.style.background = "rgba(2,6,23,.98)";
+}
+else{
+    navbar.style.background = "rgba(15,23,42,.95)";
+}
+```
+
+});
