4e875851e8e0b9900aebb89d48998dc5a178860f
Daniel Njeru <danofficial014@gmail.com>
2026-06-11 09:12:45 +0300

    Launch professional portfolio

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 window.addEventListener("scroll", () => {
 
-```
-const navbar = document.querySelector(".navbar");
+    const navbar =
+    document.querySelector(".navbar");
 
-if(window.scrollY > 50){
-    navbar.style.background = "rgba(2,6,23,.98)";
-}
-else{
-    navbar.style.background = "rgba(15,23,42,.95)";
-}
-```
+    if(window.scrollY > 50){
+
+        navbar.style.background =
+        "rgba(2,6,23,.98)";
+
+    }
+
+    else{
+
+        navbar.style.background =
+        "rgba(15,23,42,.96)";
+
+    }
 
 });
